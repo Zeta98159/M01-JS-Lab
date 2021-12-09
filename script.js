231a68ba168613f33234faf50358d4cc84f1a17d
Andrew Jorgensen <69862674+Zeta98159@users.noreply.github.com>
2021-12-09 18:38:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Your solution goes here 
+function isStrongPassword(password) {
+  // Verify long enough
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Verify does not contain "password"
+  if (password.indexOf("password") != -1) {
+    return false;
+  }
+
+  // Verify at least one uppercase letter
+  for (let i = 0; i < password.length; i++) {
+    let char = password.charCodeAt(i);
+    if (char >= 65 && char <= 90) {
+      return true;
+    }
+  }
+
+  return false
+}
